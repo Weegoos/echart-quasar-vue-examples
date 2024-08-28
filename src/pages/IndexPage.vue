a81e104481367554761f481341b27dc983a87621
Weegoos <batyr.ashim05@mail.ru>
2024-08-28 09:29:39 +0500

    Echart: Initialize the project

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>
+    <p>Index Page</p>
+  </div>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+<script>
+export default {};
 </script>
+
+<style></style>
